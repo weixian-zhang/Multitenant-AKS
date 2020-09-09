6ed0a1d809d9244b8659d5ee9608bf5f31c1967a
Code Norris <weixianzhang@outlook.com>
2020-09-09 10:20:33 +0800

    removed unwanted .sh
    masked Azure IDs

--- deploy/aks/aks-setup.sh
+++ deploy/aks/aks-setup.sh
@@ -1,12 +1,14 @@
 #setup AKS
 az aks create --resource-group rg-swiftcloud-kubernetes --name aks-swiftoffice \
---vnet-subnet-id "/subscriptions/ee611083-4581-4ba1-8116-a502d4539206/resourceGroups/rg-swiftcloud-kubernetes/providers/Microsoft.Network/virtualNetworks/vnet-k8s/subnets/subnet-k8s" \
---network-plugin azure \ #azure cni
+--vnet-subnet-id "/subscriptions/xxxxxxxxx-4581-4ba1-8116-xxxxxxxxxxxx/resourceGroups/rg-swiftcloud-kubernetes/providers/Microsoft.Network/virtualNetworks/vnet-k8s/subnets/subnet-k8s" \
+--network-plugin azure \
 --network-policy calico \
 --enable-aad --enable-azure-rbac \
---aad-admin-group-object-ids fb24271c-88c9-49b4-aa11-bac3b16433d7 \#AAD Group object id
---aad-tenant-id 72f988bf-86f1-41af-91ab-2d7cd011db47 \#AAD tenant id
+--aad-admin-group-object-ids xxxxxxxx-88c9-49b4-aa11-xxxxxxxxxxxxx \
+--aad-tenant-id 72f988bf-86f1-41af-91ab-xxxxxxxxxxxx \
 --node-count 1 --enable-addons monitoring --generate-ssh-keys
+#same as above without linebreak
+az aks create --resource-group rg-swiftcloud-kubernetes --name "aks-swiftoffice" --vnet-subnet-id /subscriptions/xxxxxxxx-4581-4ba1-8116-xxxxxxxxxxxx/resourceGroups/rg-swiftcloud-kubernetes/providers/Microsoft.Network/virtualNetworks/vnet-k8s/subnets/subnet-k8s --network-plugin azure --network-policy calico --enable-aad --enable-azure-rbac --aad-admin-group-object-ids xxxxxxxx-88c9-49b4-aa11-xxxxxxxxxxxx --aad-tenant-id xxxxxxxxx-86f1-41af-91ab-xxxxxxxxxxxx --node-count 1 --enable-addons monitoring --generate-ssh-keys
 
 az aks get-credentials --resource-group rg-swiftcloud-kubernetes --name aks-swiftoffice
 
